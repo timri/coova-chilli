timri/coova-chilli

commit bfe0e4b024735482b2db6cd67e0c4d57cf38b9b3

refactor radius packet send function

src/radius.c
@@ -690,22 +690,19 @@ int radius_timeout(struct radius_t *this) {
 
       if (RADIUS_QUEUE_HASPKT(this->queue[this->first].p)) {
 
-	/* Use the correct port for accounting and authentication */
-	if (RADIUS_QUEUE_PKT(this->queue[this->first].p,code)
-	    == RADIUS_CODE_ACCOUNTING_REQUEST)
-	  addr.sin_port = htons(this->acctport);
-	else
-	  addr.sin_port = htons(this->authport);
+        /* Use the correct port for accounting and authentication */
+        if (RADIUS_QUEUE_PKT(this->queue[this->first].p,code)
+            == RADIUS_CODE_ACCOUNTING_REQUEST) {
+          addr.sin_port = htons(this->acctport);
+        } else {
+          addr.sin_port = htons(this->authport);
+        }
 
-	if (sendto(this->fd,
-		   RADIUS_QUEUE_PKTPTR(this->queue[this->first].p),
-		   ntohs(RADIUS_QUEUE_PKT(this->queue[this->first].p, length)),
-		   0, (struct sockaddr *) &addr, sizeof(addr)) < 0) {
+        if (radius_pkt_send(this, RADIUS_QUEUE_PKTPTR(this->queue[this->first].p), &addr) < 0) {
+          radius_queue_reschedule(this, this->first);
+          return -1;    
+        }
 
-	  syslog(LOG_ERR, "%s: sendto() failed!", strerror(errno));
-	  radius_queue_reschedule(this, this->first);
-	  return -1;
-	}
       }
 
       if (radius_queue_reschedule(this, this->first)) {
@@ -1510,6 +1507,26 @@ radius_set_cb_coa_ind(struct radius_t *this,
 
 
 /*
+ * radius_pkt_send()
+ * Directly send a packet 
+ */
+int radius_pkt_send(struct radius_t *this,
+      struct radius_packet_t *pack,
+      struct sockaddr_in *peer) {
+
+  size_t len = ntohs(pack->length);
+
+  if (sendto(this->fd, pack, len, 0,(struct sockaddr *) peer,
+       sizeof(struct sockaddr_in)) < 0) {
+    syslog(LOG_ERR, "%s: sendto() failed!", strerror(errno));
+    return -1;
+  }
+
+  return 0;
+}
+
+
+/*
  * radius_req()
  * Send of a packet and place it in the retransmit queue
  */
@@ -1518,7 +1535,6 @@ int radius_req(struct radius_t *this,
 	       void *cbp)
 {
   struct sockaddr_in addr;
-  size_t len = ntohs(pack->length);
 
   /* Place packet in queue */
   if (radius_queue_in(this, pack, cbp)) {
@@ -1548,13 +1564,7 @@ int radius_req(struct radius_t *this,
          ntohs(addr.sin_port));
 #endif
 
-  if (sendto(this->fd, pack, len, 0,
-	     (struct sockaddr *) &addr, sizeof(addr)) < 0) {
-    syslog(LOG_ERR, "%s: sendto(%s) failed!", strerror(errno), inet_ntoa(addr.sin_addr));
-    return -1;
-  }
-
-  return 0;
+  return radius_pkt_send(this, pack, &addr);
 }
 
 #ifdef ENABLE_RADPROXY
@@ -1566,7 +1576,6 @@ int radius_resp(struct radius_t *this,
 		struct radius_packet_t *pack,
 		struct sockaddr_in *peer, uint8_t *req_auth) {
 
-  size_t len = ntohs(pack->length);
   struct radius_attr_t *ma = NULL; /* Message authenticator */
 
   /* Prepare for message authenticator TODO */
@@ -1582,13 +1591,7 @@ int radius_resp(struct radius_t *this,
 				this->proxysecret,
 				this->proxysecretlen);
 
-  if (sendto(this->proxyfd, pack, len, 0,
-	     (struct sockaddr *) peer, sizeof(struct sockaddr_in)) < 0) {
-    syslog(LOG_ERR, "%s: sendto() failed!", strerror(errno));
-    return -1;
-  }
-
-  return 0;
+  return radius_pkt_send(this, pack, peer);
 }
 #endif
 
@@ -1601,7 +1604,6 @@ int radius_coaresp(struct radius_t *this,
 		   struct radius_packet_t *pack,
 		   struct sockaddr_in *peer, uint8_t *req_auth) {
 
-  size_t len = ntohs(pack->length);
   struct radius_attr_t *ma = NULL; /* Message authenticator */
 
   /* Prepare for message authenticator TODO */
@@ -1617,13 +1619,7 @@ int radius_coaresp(struct radius_t *this,
 				this->secret,
 				this->secretlen);
 
-  if (sendto(this->fd, pack, len, 0,
-	     (struct sockaddr *) peer, sizeof(struct sockaddr_in)) < 0) {
-    syslog(LOG_ERR, "%s: sendto() failed!", strerror(errno));
-    return -1;
-  }
-
-  return 0;
+  return radius_pkt_send(this, pack, peer);
 }
 #endif
 

src/main-proxy.c
@@ -150,21 +150,6 @@ static proxy_request * get_request() {
   return req;
 }
 
-static int radius_reply(struct radius_t *this,
-			struct radius_packet_t *pack,
-			struct sockaddr_in *peer) {
-
-  size_t len = ntohs(pack->length);
-
-  if (sendto(this->fd, pack, len, 0,(struct sockaddr *) peer,
-	     sizeof(struct sockaddr_in)) < 0) {
-    syslog(LOG_ERR, "%s: sendto() failed!", strerror(errno));
-    return -1;
-  }
-
-  return 0;
-}
-
 static void bhex(bstring src, bstring dst) {
   int i;
   char b[4];
@@ -437,7 +422,7 @@ static int http_aaa_finish(proxy_request *req) {
       break;
   }
 
-  radius_reply(req->radius, &req->radius_res, &req->conn.peer);
+  radius_pkt_send(req->radius, &req->radius_res, &req->conn.peer);
 
   close_request(req);
 

src/main-radsec.c
@@ -43,20 +43,6 @@ static struct {
 
 } server;
 
-static int radius_reply(struct radius_t *this,
-			struct radius_packet_t *pack,
-			struct sockaddr_in *peer) {
-
-  size_t len = ntohs(pack->length);
-
-  if (sendto(this->fd, pack, len, 0,(struct sockaddr *) peer,
-	     sizeof(struct sockaddr_in)) < 0) {
-    syslog(LOG_ERR, "%s: sendto() failed!", strerror(errno));
-    return -1;
-  }
-
-  return 0;
-}
 
 static int connect_ssl(struct in_addr *addr, int port) {
   if (conn_sock(&server.conn, addr, port)) {
@@ -149,18 +135,18 @@ static void process_radius_reply() {
         case RADIUS_CODE_ACCESS_REJECT:
         case RADIUS_CODE_ACCESS_CHALLENGE:
           syslog(LOG_DEBUG, "reply auth %d", len);
-          radius_reply(server.radius_auth, &server.pack, &server.auth_peer);
+          radius_pkt_send(server.radius_auth, &server.pack, &server.auth_peer);
           break;
         case RADIUS_CODE_ACCOUNTING_RESPONSE:
           syslog(LOG_DEBUG, "reply acct %d", len);
-          radius_reply(server.radius_acct, &server.pack, &server.acct_peer);
+          radius_pkt_send(server.radius_acct, &server.pack, &server.acct_peer);
           break;
         case RADIUS_CODE_COA_REQUEST:
         case RADIUS_CODE_DISCONNECT_REQUEST:
         case RADIUS_CODE_STATUS_REQUEST:
           if (_options.coaport) {
             syslog(LOG_DEBUG, "reply coa %d", len);
-            radius_reply(server.radius_cli, &server.pack, &server.acct_peer);
+            radius_pkt_send(server.radius_cli, &server.pack, &server.acct_peer);
           }
           break;
       }

src/radius.h
@@ -299,6 +299,10 @@ int radius_set_cb_coa_ind(struct radius_t *this,
 					     struct radius_packet_t *pack,
 					     struct sockaddr_in *peer));
 
+int radius_pkt_send(struct radius_t *this,
+      struct radius_packet_t *pack,
+      struct sockaddr_in *peer);
+
 /* Send of a request */
 int radius_req(struct radius_t *this,
 	       struct radius_packet_t *pack,

commit e1145f2972a2cbe382d159800b707f2be668d294

radius proxy broken with bfe0e4b0
Description: radius proxy broken with bfe0e4b0
 bfe0e4b introduces a regression breaking the radius proxy. Replies from the
 radius proxy are incorrectly sent from this->fd instead of this->proxyfd in
 radius_resp() which results in the incorrect source port being seen by the
 AP, which rightly ignores the packet.
 .
 I'm not overly familiar with the codebase, so I'm not sure if this is the
 cleanest solution by far, but a quick hack that appears to have fixed the
 issue is as follows:
Author: nvx at github
Origin: github, https://github.com/coova/coova-chilli/issues/270
Bug: https://github.com/coova/coova-chilli/issues/270

src/radius.c
@@ -1528,6 +1528,26 @@ int radius_pkt_send(struct radius_t *this,
 
 
 /*
+ * radius_pkt_send_proxy()
+ * Directly send a packet
+ */
+int radius_pkt_send_proxy(struct radius_t *this,
+      struct radius_packet_t *pack,
+      struct sockaddr_in *peer) {
+
+  size_t len = ntohs(pack->length);
+
+  if (sendto(this->proxyfd, pack, len, 0,(struct sockaddr *) peer,
+       sizeof(struct sockaddr_in)) < 0) {
+    syslog(LOG_ERR, "%s: sendto() failed!", strerror(errno));
+    return -1;
+  }
+
+  return 0;
+}
+
+
+/*
  * radius_req()
  * Send of a packet and place it in the retransmit queue
  */
@@ -1592,7 +1612,7 @@ int radius_resp(struct radius_t *this,
 				this->proxysecret,
 				this->proxysecretlen);
 
-  return radius_pkt_send(this, pack, peer);
+  return radius_pkt_send_proxy(this, pack, peer);
 }
 #endif
 